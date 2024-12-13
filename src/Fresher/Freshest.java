2cb1164b69bd7987c9754e09825f1be0d4c054e2
misty <misty@192.168.1.170>
2024-12-13 15:44:07 -0800

    causing problems

--- src/Fresher/Freshest.java
+++ src/Fresher/Freshest.java
@@ -2,6 +2,7 @@ package Fresher;
 
 public class Freshest {
 	public static void main (String args []){
+	
 		System.out.println("Time for some review");
 		String num1 = "a";
 		String num2 = "b";
@@ -10,10 +11,13 @@ public class Freshest {
 		if ( num1 == num2) {
 			System.out.println("a is equal to 2");
 		}else {
-			System.out.println(" a is not equal to 2");
+			System.out.println("Causing issues");
 		}
+		
 	}
 	
 	
+		
+	}
 
-}
+
