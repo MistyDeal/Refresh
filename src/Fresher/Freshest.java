2ad18a9a37309095610039809d594d424dbd68d5
misty <misty@192.168.1.170>
2024-12-13 13:39:11 -0800

    code changes

--- src/Fresher/Freshest.java
+++ src/Fresher/Freshest.java
@@ -2,9 +2,17 @@ package Fresher;
 
 public class Freshest {
 	public static void main (String args []){
-		// awesome let's do this
-		//time to push again
-		System.out.println("it's time for some review");
+		System.out.println("Time for some review");
+		String num1 = "a";
+		String num2 = "b";
+	
+		if ( num1 == num2) {
+			System.out.println("a is equal to 2");
+		}else {
+			System.out.println(" a is not equal to 2");
+		}
 	}
+	
+	
 
 }
